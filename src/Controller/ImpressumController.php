b9eb3fc43b872df5d14c5c5fb3b5248f23a0658a
Stefan Warnat <ich@stefanwarnat.de>
2024-03-11 00:54:36 +0100

    add impressum, dsgvo

---
+++ src/Controller/ImpressumController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Anmeldung;
+use App\Enum\AnmeldungStatus;
+use App\Form\AnmeldungType;
+use App\Repository\RuestzeitRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Twig\Environment;
+
+class ImpressumController extends AbstractController
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+    ) {
+    }
+
+    #[Route('/impressum', name: 'impressum')]
+    public function index(Request $request, Environment $twig, RuestzeitRepository $ruestzeitRepository): Response
+    {
+        
+        return new Response($twig->render('impressum.html.twig'));
+    }
+}
