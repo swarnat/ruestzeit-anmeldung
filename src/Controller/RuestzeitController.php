13773327e84b6adc2459664f9f4481525561a1df
Stefan Warnat <ich@stefanwarnat.de>
2024-04-11 00:27:55 +0200

    Implement registration confirmation to owner

--- src/Controller/RuestzeitController.php
+++ src/Controller/RuestzeitController.php
@@ -8,9 +8,13 @@ use App\Form\AnmeldungType;
 use App\Repository\RuestzeitRepository;
 use App\Service\PostalcodeService;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 use Twig\Environment;
 
@@ -23,9 +27,15 @@ class RuestzeitController extends AbstractController
     }
 
     #[Route('/', name: 'homepage')]
-    public function index(Request $request, Environment $twig, RuestzeitRepository $ruestzeitRepository): Response
+    public function index(Request $request, Environment $twig, RuestzeitRepository $ruestzeitRepository, MailerInterface $mailer): Response
     {
         $ruestzeit = $ruestzeitRepository->findOneBy([]);
+
+        $allowRegistration = false;
+
+        if($request->get('pw', null) === "anmeldung-saatkorn") {
+            $allowRegistration = true;
+        }
 
         $anmeldung = new Anmeldung();
         $form = $this->createForm(AnmeldungType::class, $anmeldung);
@@ -60,15 +70,40 @@ class RuestzeitController extends AbstractController
             $this->entityManager->persist($anmeldung);
             $this->entityManager->flush();
 
+            $email = (new TemplatedEmail())
+                ->from('no-reply@kirche-hohndorf.de')
+                ->to('ich@stefanwarnat.de')
+                ->htmlTemplate('emails/anmeldung.html.twig')
+                ->locale('de')
+                ->subject('[' . $ruestzeit->getTitle() . '] Anmeldung ' . $anmeldung->getLastname() . ', ' . $anmeldung->getFirstname() . ' [' . $anmeldung->getStatus()->value . ']')
+                ->context([
+                        'anmeldung' => $anmeldung,
+                    ]);
+
+            try {
+                $debug = $mailer->send($email);
+                var_dump($debug);
+            } catch (TransportExceptionInterface $e) {
+                var_dump($e);
+                // some error prevented the email sending; display an
+                // error message or try to resend the message
+            }
+                    
             flash()->addSuccess('Die Anmeldung wurde erfolgreich gespeichert. Vielen Dank!', 'Erfolgreich', [
                 'timeout' => 30000
             ]);
 
-            return $this->redirectToRoute('homepage');
+            if($allowRegistration) {
+                return $this->redirectToRoute('homepage', ["pw" => "anmeldung-saatkorn"]);
+            } else {
+                return $this->redirectToRoute('homepage');
+            }
+            
         }
 
         return new Response($twig->render('ruestzeit/index.html.twig', [
             'ruestzeit' => $ruestzeit,
+            'allowRegistration' => $allowRegistration,
             'form' => !empty($formView) ? $formView : [], 
         ]));
     }
