7dc3c83f0020ac4b56293c6b8226dc5f615b9f52
Stefan Warnat <ich@stefanwarnat.de>
2025-02-24 23:05:36 +0100

    feat: add option for a maintenance mode

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ php bin/console doctrine:schema:update --force
 ## Migrate
 
 php bin/console doctrine:migrations:migrate --no-interaction
+
+## Maintenance Mode
+
+Create a file .maintenance in Root directory to disable the frontend functions completely.  
+To allow single IPs to bypass the maintenance mode, just write this ip one per line into .maintenance file.  
+You can also define a Cookie, called "maintenance_bypass" and set that to "1" to bypass maintenance mode.
--- public/index.php
+++ public/index.php
@@ -1,5 +1,39 @@
 <?php
 
+if(is_file(dirname(__DIR__) . DIRECTORY_SEPARATOR . ".maintenance")) {
+    
+    if(!function_exists("getClientIPMaintenance")) {
+        function getClientIPMaintenance() {
+            $headers = [
+                'HTTP_X_FORWARDED_FOR',
+                'HTTP_CLIENT_IP',
+                'HTTP_X_REAL_IP',
+                'REMOTE_ADDR'
+            ];
+        
+            foreach ($headers as $header) {
+                if (!empty($_SERVER[$header])) {
+                    $ipList = explode(',', $_SERVER[$header]);
+                    return trim($ipList[0]); // Erste IP ist die ursprüngliche Client-IP
+                }
+            }
+        
+            return 'Unbekannte IP';
+        }
+    }
+    
+    $clientIP = getClientIPMaintenance();
+
+    $content = file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . ".maintenance");
+    
+    if(strpos($content, $clientIP) === false && empty($_COOKIE["maintenance_bypass"])) {
+
+        readfile(dirname(__DIR__) . DIRECTORY_SEPARATOR . "templates" . DIRECTORY_SEPARATOR . "maintenance.html");
+        exit();
+
+    }
+}
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
