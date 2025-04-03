e729d2e1e020919fd0927a135ce4e73567c126ff
Stefan Warnat <ich@stefanwarnat.de>
2025-04-03 23:54:04 +0200

    fix: create user during first setup

--- migrations/Version20240309105847.php
+++ migrations/Version20240309105847.php
@@ -12,6 +12,7 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20240309105847 extends AbstractMigration
 {
+    
     public function getDescription(): string
     {
         return '';
@@ -22,7 +23,23 @@ final class Version20240309105847 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE admin (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_2D4F16D3989D9B62 ON ruestzeit (slug)');
+
     }
+
+    public function postUp(Schema $schema): void
+    {
+        # ruestzeit-admin
+        $password = '$2y$10$dnnaqeM0UzZ3wQ/bvJMJkelC5bm4o67XOW5yQWSn0rrQMvGVWuN1G';
+
+        $this->connection->insert("admin", [
+            "username" => "ruestzeit-admin",
+            "password" => $password,
+            "roles" => json_encode(["ROLE_ADMIN"])
+        ]);
+
+    }    
+
+
 
     public function down(Schema $schema): void
     {
@@ -30,4 +47,5 @@ final class Version20240309105847 extends AbstractMigration
         $this->addSql('DROP TABLE admin');
         $this->addSql('DROP INDEX UNIQ_2D4F16D3989D9B62 ON ruestzeit');
     }
+  
 }
